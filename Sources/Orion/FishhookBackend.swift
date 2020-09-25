15f0385f75e5362eda01e92c52aa5c3b6eb97533
kabiroberai <oberai.kabir@gmail.com>
2020-09-25 05:54:57 +0530

    Fix memory issues in FishookBackend

--- Sources/Orion/FishhookBackend.swift
+++ Sources/Orion/FishhookBackend.swift
@@ -1,41 +1,6 @@
 import Foundation
 import OrionC
 
-// https://github.com/apple/swift/blob/cc511c23254f7203e2f818c5e4f3298ce9497c71/stdlib/private/SwiftPrivate/SwiftPrivate.swift#L32
-// Compute the prefix sum of `seq`.
-private func scan<S: Sequence, U>(_ seq: S, _ initial: U, _ combine: (U, S.Element) -> U) -> [U] {
-    var result: [U] = []
-    result.reserveCapacity(seq.underestimatedCount)
-    var runningResult = initial
-    for element in seq {
-        runningResult = combine(runningResult, element)
-        result.append(runningResult)
-    }
-    return result
-}
-
-// more efficient than mapping with strdup because this creates one large allocation rather than multiple small ones.
-// modified to use UInt8 instead of CChar, and also to not include the final NULL element
-// https://github.com/apple/swift/blob/cc511c23254f7203e2f818c5e4f3298ce9497c71/stdlib/private/SwiftPrivate/SwiftPrivate.swift#L60
-private func withArrayOfCStrings<S: Sequence, R>(
-    _ args: S, _ body: ([UnsafeMutablePointer<UInt8>?]) -> R
-) -> R where S.Element == String {
-    // dropLast removes the extra element at the end which we could otherwise set to NULL
-    let argsCounts = args.lazy.dropLast().map { $0.utf8.count + 1 }
-    let argsOffsets = [0] + scan(argsCounts, 0, +)
-    let argsBufferSize = argsOffsets.last!
-    var argsBuffer: [UInt8] = []
-    argsBuffer.reserveCapacity(argsBufferSize)
-    for arg in args {
-        argsBuffer.append(contentsOf: arg.utf8)
-        argsBuffer.append(0)
-    }
-    return argsBuffer.withUnsafeMutableBufferPointer { argsBuffer in
-        let ptr = argsBuffer.baseAddress!
-        return body(argsOffsets.map { ptr + $0 })
-    }
-}
-
 public struct FishhookBackend<UnderlyingBackend: Backend>: Backend {
     let underlyingBackend: UnderlyingBackend
 
@@ -55,41 +20,39 @@ public struct FishhookBackend<UnderlyingBackend: Backend>: Backend {
         fileprivate var requests: [Request] = []
     }
 
-    private func apply(
-        requests: [Builder.Request],
-        symbols: [UnsafeMutablePointer<UInt8>?],
-        origs: UnsafeMutableBufferPointer<UnsafeMutableRawPointer?>
-    ) {
+    private func apply(requests: [Builder.Request]) {
+        guard !requests.isEmpty else { return }
+
+        // we need to keep this around, so we don't deallocate it. See comment in rebinding.init call
+        // for rationale.
+        let origs = UnsafeMutableBufferPointer<UnsafeMutableRawPointer?>.allocate(capacity: requests.count)
+
         // NOTE: Unfortunately fishhook doesn't support specifying a symbol's image (although
         // there's a fork that does; maybe we should consider using it?). While it may seem like
         // rebind_symbols_image does this job, its purpose is in fact to only make the hook apply
         // to callers in that image, and not to specify the image of the target symbol itself.
 
-        var rebindings = symbols.enumerated().map { idx, sym in
+        var rebindings = requests.enumerated().map { idx, request in
             rebinding(
-                name: UnsafeRawPointer(sym!).assumingMemoryBound(to: Int8.self),
-                replacement: requests[idx].replacement,
+                // Turns out fishhook doesn't copy this string so we're responsible for keeping
+                // it alive, because the rebindings are stored globally, and are accessed not
+                // only when rebind_symbols is called but also every time a new image is added.
+                // While we could use a dict to keep a single copy of each symbol alive, blindly
+                // calling strdup is alright since each hooked symbol is most likely unique anyway.
+                name: strdup(request.symbol),
+                replacement: request.replacement,
+                // this is also stored globally, as mentioned above. It appears that fishhook writes
+                // to this each time an image is processed, passing in that image's orig stub. Even
+                // though this orig is later discarded, we can't pass NULL because we do check it in
+                // order to know whether hooking was successful
                 replaced: origs.baseAddress! + idx
             )
         }
 
-        guard orion_rebind_symbols(&rebindings, symbols.count) == 0
+        guard orion_rebind_symbols(&rebindings, rebindings.count) == 0
             else { fatalError("Failed to hook functions") }
-    }
 
-    private func apply(requests: [Builder.Request]) {
-        if !requests.isEmpty {
-            let symbols = requests.lazy.map { $0.symbol }
-            var origs: [UnsafeMutableRawPointer?] = Array(repeating: nil, count: requests.count)
-
-            withArrayOfCStrings(symbols) { rawSymbols in
-                origs.withUnsafeMutableBufferPointer { rawOrigs in
-                    apply(requests: requests, symbols: rawSymbols, origs: rawOrigs)
-                }
-            }
-
-            requests.enumerated().forEach { $1.completion(origs[$0]) }
-        }
+        zip(requests, origs).forEach { $0.completion($1) }
     }
 
     public func hook(_ build: (inout Builder) -> Void) {
