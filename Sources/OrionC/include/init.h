d0f59ece51b403f7e2930e43d2944edf7711873e
kabiroberai <oberai.kabir@gmail.com>
2020-12-23 03:30:52 +0530

    Update headers to support cpp

--- Sources/OrionC/include/init.h
+++ Sources/OrionC/include/init.h
@@ -2,11 +2,17 @@
 #define INIT_H_
 
 // this is only required in SPM mode but guarding it behind
-// a conditional confuses the compiler
+// a conditional confuses the compiler. Don't call it yourself.
 void _orion_init_c(void);
 
 // this symbol should always be found while linking; it's
 // declared in the Orion glue file as @_cdecl("orion_init")
+#ifdef __cplusplus
+extern "C" {
+#endif
 void orion_init(void);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- Sources/OrionC/include/super.h
+++ Sources/OrionC/include/super.h
@@ -2,6 +2,8 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+// These functions underpin Orion's `supr` proxy. Don't call them yourself.
+
 void _orion_with_objc_super(id receiver, Class cls, void(NS_NOESCAPE ^block)(void *super_struct, void *send));
 
 #ifndef __arm64__
