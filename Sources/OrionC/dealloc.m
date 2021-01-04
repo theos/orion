3b0ccb9c3a18d08c7538c4910e6cd30060d05f28
kabiroberai <oberai.kabir@gmail.com>
2021-01-05 03:58:51 +0530

    Remove commented code from dealloc.{m,h}

--- Sources/OrionC/dealloc.m
+++ Sources/OrionC/dealloc.m
@@ -12,19 +12,3 @@ void _orion_call_super_dealloc(Class cls, const CFTypeRef *self, SEL _cmd) {
     struct objc_super sup = { (__bridge __unsafe_unretained id)(*self), cls };
     objc_msgSendSuper2(&sup, _cmd);
 }
-
-//_orion_DeallocMethod _orion_createDeallocMethod(SEL deallocSel, _orion_DeallocPolicy(^block)(id)) {
-//    // without __unsafe_unretained we would strongly retain self and break the
-//    // dealloc method
-//    return (_orion_DeallocMethod)imp_implementationWithBlock(^(id __unsafe_unretained self) {
-//        _orion_DeallocPolicy policy = block(self);
-//        switch (policy.kind) {
-//            case _orion_DeallocPolicyKindCallOrig:
-//                ((_orion_DeallocMethod)policy.param)(self, deallocSel);
-//                break;
-//            case _orion_DeallocPolicyKindCallSupr:
-//                objc_msgSendSuper2(&(struct objc_super) { self, (__bridge Class)policy.param }, deallocSel);
-//                break;
-//        }
-//    });
-//}
