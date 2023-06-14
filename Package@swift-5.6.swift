9e7333bb9d529b4c41a43248fbdb5d2daf69cb0f
Kabir Oberai <oberai.kabir@gmail.com>
2023-06-13 21:02:54 -0400

    Allow swiftc forwards compat

--- Package@swift-5.6.swift
+++ Package@swift-5.6.swift
@@ -3,24 +3,20 @@
 import PackageDescription
 import Foundation
 
-enum Builder {
-    case theos
-    case xcode
-    case spm
-}
-
 let swiftSyntax: Package.Dependency = {
+    let url = "https://github.com/apple/swift-syntax"
     #if swift(>=5.9)
-    #error("""
-    Orion does not support this version of Swift yet. \
+    #warning("""
+    Orion does not officially support this version of Swift yet. \
     Please check https://github.com/theos/Orion for progress updates.
     """)
-    #elseif swift(>=5.8)
-    return .package(url: "https://github.com/apple/swift-syntax", from: "508.0.0")
+    #endif
+    #if swift(>=5.8)
+    return .package(url: url, from: "508.0.0")
     #elseif swift(>=5.7)
-    return .package(url: "https://github.com/apple/swift-syntax", exact: "0.50700.0")
+    return .package(url: url, exact: "0.50700.0")
     #elseif swift(>=5.6)
-    return .package(url: "https://github.com/apple/swift-syntax.git", exact: "0.50600.1")
+    return .package(url: url, exact: "0.50600.1")
     #else
     #error("""
     Internal error: Swift Package Manager should be reading from
@@ -34,65 +30,6 @@ let macPlatform: SupportedPlatform = {
     return .macOS("10.15")
     #else
     return .macOS("10.12")
-    #endif
-}()
-
-let builder: Builder
-let env = ProcessInfo.processInfo.environment
-if env["SPM_THEOS_BUILD"] == "1" {
-    builder = .theos
-} else if env["XPC_SERVICE_NAME"]?.contains("com.apple.dt.Xcode.") == true {
-    builder = .xcode
-} else {
-    builder = .spm
-}
-
-func system(_ path: String, _ args: String...) -> String {
-    let pipe = Pipe()
-    let process = Process()
-    process.launchPath = path
-    process.arguments = args
-    process.standardOutput = pipe
-    process.standardError = nil
-    process.launch()
-    process.waitUntilExit()
-    return String(data: pipe.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8)!
-        .trimmingCharacters(in: .whitespacesAndNewlines)
-}
-
-// allow lib_InternalSwiftSyntaxParser to be located at runtime. Based on
-// https://github.com/muter-mutation-testing/muter/blob/dc53a9cd1792b2ffd3c9a1a0795aae99e8c7334d/Package.swift#L40
-let rpathLinkerSettings: [LinkerSetting]? = {
-    #if os(macOS)
-    let xcrunSwiftPath = system("/usr/bin/xcrun", "-f", "swift")
-    let overriddenPrefix = URL(fileURLWithPath: xcrunSwiftPath)
-        .deletingLastPathComponent().deletingLastPathComponent()
-
-    let defaultPlatformPath = system("/usr/bin/xcodebuild", "-version", "-sdk", "macosx", "PlatformPath")
-    let defaultPrefix = URL(fileURLWithPath: defaultPlatformPath)
-        .deletingLastPathComponent().deletingLastPathComponent()
-        .appendingPathComponent("Toolchains/XcodeDefault.xctoolchain/usr")
-
-    let computedPrefix: URL
-    if overriddenPrefix == defaultPrefix {
-        // there's no explicit toolchain override. A more specific override may exist
-        // inside the PATH
-        let xcodebuildPath = system("/usr/bin/type", "-p", "xcodebuild")
-        let platformPath = system(xcodebuildPath, "-version", "-sdk", "macosx", "PlatformPath")
-        computedPrefix = URL(fileURLWithPath: platformPath)
-            .deletingLastPathComponent().deletingLastPathComponent()
-            .appendingPathComponent("Toolchains/XcodeDefault.xctoolchain/usr")
-    } else {
-        computedPrefix = overriddenPrefix
-    }
-
-    let rpath = computedPrefix.appendingPathComponent("lib/swift/macosx")
-    return [
-        .unsafeFlags(["-Xlinker", "-rpath", "-Xlinker", rpath.path])
-    ]
-    #else
-    // TODO: Do we need an rpath here too?
-    return nil
     #endif
 }()
 
@@ -129,13 +66,11 @@ var package = Package(
             dependencies: [
                 "OrionProcessor",
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
-            ],
-            linkerSettings: rpathLinkerSettings
+            ]
         ),
         .testTarget(
             name: "OrionProcessorTests",
-            dependencies: ["OrionProcessor"],
-            linkerSettings: rpathLinkerSettings
+            dependencies: ["OrionProcessor"]
         ),
         .plugin(
             name: "OrionPlugin",
@@ -146,7 +81,7 @@ var package = Package(
 )
 
 #if canImport(ObjectiveC)
-if builder != .theos {
+if ProcessInfo.processInfo.environment["SPM_THEOS_BUILD"] != "1" {
     package.products += [
         .library(
             name: "Orion",
